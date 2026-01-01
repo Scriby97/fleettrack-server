90ce958c9c067304899c565c98ce44d735afa0b8
Nicolas Balmer <nicolas_balmer@hotmail.com>
2026-01-01 14:10:40 +0100

    fixed build error

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import { AuthModule } from './auth/auth.module';
       type: 'postgres',
       url: process.env.DATABASE_URL,
       host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT) || 5432,
+      port: parseInt(process.env.DB_PORT || '5432'),
       username: process.env.DB_USERNAME || 'postgres',
       password: process.env.DB_PASSWORD || 'SkiweltGstaad',
       database: process.env.DB_NAME || 'postgres',
