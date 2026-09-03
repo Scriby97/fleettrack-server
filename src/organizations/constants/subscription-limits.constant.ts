88cb2ade90f75b5e32df234e6d8aa8168956cf82
Nicolas Balmer <nicolas_balmer@hotmail.com>
2026-09-03 20:38:38 +0200

    Changed subscription prices

--- src/organizations/constants/subscription-limits.constant.ts
+++ src/organizations/constants/subscription-limits.constant.ts
@@ -8,6 +8,6 @@ export interface SubscriptionLimits {
 
 export const SUBSCRIPTION_LIMITS: Record<SubscriptionTier, SubscriptionLimits> = {
   [SubscriptionTier.LIEUTENANT]: { maxVehicles: 2, maxMembers: 5, priceChf: 0 },
-  [SubscriptionTier.CAPTAIN]: { maxVehicles: 20, maxMembers: 50, priceChf: 99 },
-  [SubscriptionTier.GENERAL]: { maxVehicles: null, maxMembers: null, priceChf: 199 },
+  [SubscriptionTier.CAPTAIN]: { maxVehicles: 20, maxMembers: 50, priceChf: 49 },
+  [SubscriptionTier.GENERAL]: { maxVehicles: null, maxMembers: null, priceChf: 99 },
 };
--- src/organizations/enums/subscription-tier.enum.ts
+++ src/organizations/enums/subscription-tier.enum.ts
@@ -1,7 +1,7 @@
 export enum SubscriptionTier {
   LIEUTENANT = 'lieutenant', // Free - bis 2 Fahrzeuge, 5 Mitarbeiter
-  CAPTAIN = 'captain', // CHF 99.-/Monat - bis 20 Fahrzeuge, 50 Mitarbeiter
-  GENERAL = 'general', // CHF 199.-/Monat - unlimitiert
+  CAPTAIN = 'captain', // CHF 49.-/Monat - bis 20 Fahrzeuge, 50 Mitarbeiter
+  GENERAL = 'general', // CHF 99.-/Monat - unlimitiert
 }
 
 export enum SubscriptionStatus {
